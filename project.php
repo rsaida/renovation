0d3e032e9807f380163986a6a3a02358769e93ad
Sherzod Sobirov <sherzod02sobirov@mail.ru>
2025-02-09 21:37:14 +0300

    project

--- project.php
+++ project.php
@@ -1,63 +1,151 @@
 <?php
-     require_once "db.php";
-     if (isset($_GET["id"])) {
-          $id = $_GET["id"];
-          $photos = getphotos($id);
-          $pic = getDisplayedPhoto($id)[0];
-     }
+require_once "db.php";
+if (isset($_GET["id"])) {
+     $id = $_GET["id"];
+     $photos = getphotos($id);
+     $pic = !empty($photos) ? $photos[0]['path'] : '';
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Document</title>
+     <title>Home Page</title>
+     <script defer src="script.js"></script>
      <link rel="stylesheet" href="style.css">
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+     <link href="https://fonts.googleapis.com/css2?family=Kalnia:wght@100..700&display=swap" rel="stylesheet">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
      <link rel="stylesheet" href="services.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Kalnia:wght@100..700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-
      <style>
-          .small img {
-               width: auto;
-               height: 500px;
-
-               padding-top: 10px;
+          body {
+               background-color: white;
           }
-          .small {
+
+          #gallery-container {
+               padding-top: 50px;
+               display: flex;
                margin: 0 auto;
                text-align: center;
+               align-items: center;
+               padding-left: 100px;
+               padding-right: 100px;
+               max-height: 700px;
+               height: 700px;
+               overflow: hidden;
+          }
+
+          #displayImage {
+               position: relative;
+               width: 100%;
+               height: 700px;
+               display: flex;
+               align-items: center;
+               justify-content: center;
+               overflow: hidden;
+          }
+          #displayImage img {
+               position: absolute;
+               max-height: 700px;
+               height: 700px;
+               width: auto;
+               object-fit: cover;
+               top: 0;
+               left: 0;
+               transition: opacity 0.2s ease-in-out;
+               max-width: none;
+               /* Ensure images don't scale down */
+          }
+
+          #mainImage {
+               z-index: 1;
+               opacity: 1;
+          }
+
+          #nextImage {
+               z-index: 2;
+               opacity: 0;
+          }
+
+          .nav-arrow {
+               color: rgb(70, 70, 70);
+               font-size: 24px;
+               cursor: pointer;
+               text-decoration: none;
+               padding: 10px;
           }
      </style>
+
 </head>
+
 <body>
-     <?php
-            include 'topbar.php';
-            renderHeader();
-     ?>
-     <br>
-     
-     <div id="servicesDiv">
+     <div id="mains">
+          <?php
+          include 'topbar.php';
+          renderHeader();
+          ?>
+     </div>
+
+     <div id="gallery-container">
+          <a href="#" class="nav-arrow prev" onclick="showPrevImage(event)">&#9665;</a>
           <div id="displayImage">
-               <img src="<?=$pic;?>" alt="">
+               <img id="mainImage" src="<?= $pic; ?>" alt="Displayed Image">
+               <img id="nextImage" src="" alt="Next Image">
           </div>
-          <div id="servicesText"style="text-align: right;">
-               <h1 >Project Info</h1> <br>
-               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia vel animi explicabo libero obcaecati dignissimos non sit veniam dolore enim ea at, esse aperiam delectus laudantium. Minima omnis fugit nostrum!</p> <br>
-               <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem a explicabo consequatur ducimus fugit harum itaque eos atque quaerat aperiam voluptate facilis est, facere quibusdam iure, dignissimos odio, exercitationem excepturi?</p>
-          </div>
+          <a href="#" class="nav-arrow next" onclick="showNextImage(event)">&#9655;</a>
      </div>
-     <!-- <a href="projects.php">back</a> -->
-     <br>
-     <div class = "small">
-     <?php
-          foreach($photos as $i) {
-               echo '<img src="', $i['path'], '" alt="???picnotloaded??? width="640" height="480"><br>';
-          }
-     ?>
-     </div>
+
+     <script>
+          document.addEventListener("DOMContentLoaded", function () {
+               let images = <?php echo json_encode(array_column($photos, 'path')); ?>;
+               let currentIndex = 0;
+               const mainImage = document.getElementById("mainImage");
+               const nextImage = document.getElementById("nextImage");
+               let isTransitioning = false;
+
+               function crossFade(newIndex) {
+                    if (isTransitioning) return;
+                    isTransitioning = true;
+
+                    nextImage.src = images[newIndex];
+                    nextImage.style.opacity = 1;
+
+                    setTimeout(() => {
+                         mainImage.src = images[newIndex];
+                         nextImage.style.opacity = 0;
+                         currentIndex = newIndex;
+                         isTransitioning = false;
+                    }, 200);
+               }
+
+               function updateImage(index) {
+                    const newIndex = (index + images.length) % images.length;
+                    crossFade(newIndex);
+               }
+
+               function showNextImage(e) {
+                    e.preventDefault();
+                    updateImage(currentIndex + 1);
+               }
+
+               function showPrevImage(e) {
+                    e.preventDefault();
+                    updateImage(currentIndex - 1);
+               }
+
+               document.addEventListener("keydown", function (event) {
+                    if (event.key === "ArrowRight") {
+                         showNextImage(event);
+                    } else if (event.key === "ArrowLeft") {
+                         showPrevImage(event);
+                    }
+               });
+          });
+     </script>
 </body>
+
 </html>
